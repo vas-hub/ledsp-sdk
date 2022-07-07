be2a34f2644b04487e0c17f211dfc2ac778703fb
Davide Giardini <davide.giardini@vas.it>
2022-07-07 10:16:42 +0200

    Add ledsp play info type (validation is still tbi)

--- src/ledsp-encoding.ts
+++ src/ledsp-encoding.ts
@@ -1,9 +1,19 @@
 export const QSDecode = (encodedParam: string) =>
+  // TODO runtime validation?
   JSON.parse(
     Buffer.from(decodeURIComponent(encodedParam), "base64").toString("ascii")
-  );
+  ) as LedspPlayInfo;
 
-export const QSEncode = (paramToEncode: object) =>
+export const QSEncode = (paramToEncode: LedspPlayInfo) =>
+  // TODO runtime validation?
   encodeURIComponent(
     Buffer.from(JSON.stringify(paramToEncode)).toString("base64")
   );
+
+type LedspPlayInfo = {
+  gameId: string;
+  playerId: string;
+  interpretationId: string;
+  team: string;
+  session: string;
+};
