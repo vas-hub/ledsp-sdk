8883f7855a391b98fc5eaf35d527574285a7ea99
Davide Giardini <davide.giardini@vas.it>
2022-07-20 11:08:43 +0200

    Add emulation to game progress events and change the client interface

--- src/ledsp-client.ts
+++ src/ledsp-client.ts
@@ -6,26 +6,28 @@ import { GameConcept, Interpretation, Observation } from "./interfaces";
 
 export default class LedspClient {
   ledspHttpClient: HttpClient;
+  ledspEmulator: LedspEmulator;
 
-  constructor(
-    public readonly environment: string,
-    private readonly gameConceptToEmulate?: GameConcept
-  ) {
-    if (!LEDSP_API_ENDPOINT[environment])
-      throw new Error(`Unknown environment for ledsp-sdk: ${environment}`);
+  constructor(private readonly config: LedspClientConfig) {
+    if (!LEDSP_API_ENDPOINT[config.environment])
+      throw new Error(
+        `Unknown environment for ledsp-sdk: ${config.environment}`
+      );
+
+    if (config.emulate && !config.gameConceptToEmulate)
+      throw new Error(`
+      You must provide a game concept to emulate.`);
 
     this.ledspHttpClient = new HttpClient(
-      LEDSP_API_ENDPOINT[this.environment].concat("/", LEDSP_API_BASEPATH)
+      LEDSP_API_ENDPOINT[config.environment].concat("/", LEDSP_API_BASEPATH)
     );
+    this.ledspEmulator = new LedspEmulator(config.gameConceptToEmulate);
   }
 
   async findInterpretation(interpretationId: string): Promise<Interpretation> {
     // TODO implement a specific class and transform it to a Decorator
-    if (this.gameConceptToEmulate)
-      return LedspEmulator.findInterpretation(
-        interpretationId,
-        this.gameConceptToEmulate
-      );
+    if (this.config.emulate)
+      return this.ledspEmulator.findInterpretation(interpretationId);
 
     return await this.ledspHttpClient.get(
       `game-launcher/interpretations/${interpretationId}`
@@ -38,15 +40,7 @@ export default class LedspClient {
    * @param event {GameProgressEvent} - The event's payload.
    */
   async sendGameProgressEvent(event: GameProgressEvent): Promise<void> {
-    // TODO implement a specific class and transform it to a Decorator
-    if (this.gameConceptToEmulate) return;
-
-    if (!GAME_PROGRESS_EVENT_TYPES.includes(event.eventType))
-      throw new Error(
-        `Game Progress Event: unknown event type: ${event.eventType}`
-      );
-
-    return await this.ledspHttpClient.post("games-progresses", {
+    const eventToSend = {
       ...event,
       id: `${event.gameId}.${
         typeof window !== "undefined" &&
@@ -56,7 +50,22 @@ export default class LedspClient {
           : Math.ceil(Math.random() * Date.now()).toString(36)
       }`,
       timestamp: Date.now(),
-    });
+    };
+
+    // TODO implement a specific class and transform it to a Decorator
+    if (this.config.emulate)
+      return this.ledspEmulator.sendGameProgressEvent(eventToSend);
+
+    if (!GAME_PROGRESS_EVENT_TYPES.includes(event.eventType))
+      throw new Error(
+        `Game Progress Event: unknown event type: ${event.eventType}`
+      );
+
+    return await this.ledspHttpClient.post("games-progresses", eventToSend);
+  }
+
+  get gameProgressEvents(): GameProgressEvent[] {
+    return this.ledspEmulator.events;
   }
 
   async saveResults(results: Observation[]) {}
@@ -64,10 +73,16 @@ export default class LedspClient {
   // TODO: Check: should this endpoint stay here?
   // TODO: Type this.
   async debriefingInfo(debriefingId: string) {
-    if (this.gameConceptToEmulate) return;
+    if (this.config.gameConceptToEmulate) return;
 
     return this.ledspHttpClient.get(
       `game-results-storages/payloads?gamingSessionIds[]=${debriefingId}`
     );
   }
 }
+
+type LedspClientConfig = {
+  environment: string;
+  emulate?: boolean;
+  gameConceptToEmulate?: GameConcept;
+};
